acd01f2b6ebd9459b9e4b500e164ba39840ccd53
jboktor <jboktor@caltech.edu>
2026-02-16 13:18:15 -0800

    Update SeqFISH notebook with new data processing steps for MapMyCells annotation, including gene metadata handling and conversion to h5ad format. Adjust SLURM scripts for output paths and resource specifications. Introduce dynamic SLURM script for flexible execution.

--- notebooks/shell_scripts/slurm-map-my-cells-dynamic.sh
+++ notebooks/shell_scripts/slurm-map-my-cells-dynamic.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --cpus-per-task=24
-#SBATCH --mem=300G
-#SBATCH --time=12:00:00   # time limit
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=100G
+#SBATCH --time=6:00:00   # time limit
 #SBATCH --partition=expansion
 #SBATCH --mail-user=jboktor@caltech.edu   # email address
 #SBATCH --mail-type=FAIL   # Notify on failure.
@@ -15,8 +15,16 @@ source activate mapmycells
 module load cuda/12.2.1-gcc-11.3.1-sdqrj2e
 
 ref_path="/resnick/groups/MazmanianLab/jboktor/WILDRxSPF_brains/data/input/ref_genomes/MapMyCellsRefs"
-output_path="/resnick/groups/MazmanianLab/jboktor/WILDRxSPF_brains/data/interim/MapMyCells_trial"
-anndata_path="/resnick/groups/MazmanianLab/jboktor/WILDRxSPF_brains/data/input/parse_bio_anndata/anndata_ensembl.h5ad"
+# output_path="/resnick/groups/MazmanianLab/jboktor/WILDRxSPF_brains/data/interim/MapMyCells_trial"
+# anndata_path="/resnick/groups/MazmanianLab/jboktor/WILDRxSPF_brains/data/input/parse_bio_anndata/anndata_ensembl.h5ad"
+
+while getopts "i:o:" opt; do
+  case $opt in
+    i) anndata_path="$OPTARG";;
+    o) output_path="$OPTARG";;
+    \?) echo "Invalid option -$OPTARG" >&2; exit 1;
+  esac
+done
 
 mkdir -p $output_path
 
@@ -24,13 +32,13 @@ python -m cell_type_mapper.cli.from_specified_markers \
 --query_path $anndata_path \
 --query_markers.serialized_lookup $ref_path/mouse_markers_230821.json \
 --precomputed_stats.path $ref_path/precomputed_stats_ABC_revision_230821.h5 \
---extended_result_path $output_path/output.json \
---csv_result_path $output_path/output.csv \
---log_path $output_path/log.txt \
+--extended_result_path $output_path/mapmycells_output.json \
+--csv_result_path $output_path/mapmycells_output.csv \
+--log_path $output_path/mapmycells_log.txt \
 --drop_level CCN20230722_SUPT \
 --cloud_safe False \
 --type_assignment.normalization raw \
---type_assignment.n_processors 24 \
---type_assignment.chunk_size 25000 \
+--type_assignment.n_processors 4 \
+--type_assignment.chunk_size 10000 \
 --type_assignment.rng_seed 42 \
 --tmp_dir /resnick/scratch/jbok/tmp
